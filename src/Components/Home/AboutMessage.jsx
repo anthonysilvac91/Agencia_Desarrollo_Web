55a7133096f72cd521f30c7eb17d337d75508c94
Anthony Silva <anthony.silvac@gmail.com>
2025-02-09 20:30:26 -0300

    se completa responsive

--- src/Components/Home/AboutMessage.jsx
+++ src/Components/Home/AboutMessage.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const AboutMessage = () => {
   return (
-    <div className='md:mb-10 md:my-10  '>
-        <div className='md:gap-40 grid grid-cols-3 responsive-padding  lg:gap-10 items-center'>
-            <div className='w-72'>
+    <div className='sm:mb-10 sm:my-5 md:my-10  my-2'>
+        <div className='flex flex-col sm:gap-40 md:gap-40 sm:grid sm:grid-cols-3 responsive-padding  lg:gap-10 items-center'>
+            <div className='w-32 sm:w-48 md:w-72'>
             <img src="https://res.cloudinary.com/djwxwsnel/image/upload/v1737908291/luz_bgpyyv.png" alt="" />
             </div>
-            <p className='md:text-2xl lg:text-xl xl:text-2xl col-span-2'>
+            <p className='mb-5 sm:mb-0 text-sm sm:text-2xl lg:text-xl xl:text-2xl col-span-2'>
             Tu negocio necesita más que buenas ideas, necesita una estrategia clara y efectiva. Una que no solo te diga qué hacer, sino cómo y cuándo hacerlo
             </p>
             
             
 
         </div>
-        <div className='text-white bg-blue-dark grid grid-cols-3 responsive-padding lg:gap-10 md:py-10 items-center'>
-        <h2 className='md:text-5xl lg:text-6xl xl:text-7xl tracking-[0.2em] col-span-2 leading-relaxed'>LLEGA A LO MÁS ALTO</h2>
+        <div className='text-white bg-blue-dark flex flex-col sm:grid sm:grid-cols-3 responsive-padding gap-2 sm:gap-0 py-4 lg:gap-10 sm:py-5 md:py-10 items-center'>
+        <h2 className='text-xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl tracking-[0.2em] col-span-2 leading-relaxed'>LLEGA A LO MÁS ALTO</h2>
             
-            <p className=' md:text-lg lg:text-xl xl:text-2xl '>
+            <p className='text-sm sm:text-sm md:text-lg lg:text-xl xl:text-2xl '>
             Nuestra Agencia Digital es el aliado perfecto para impulsar tu marca y aumentar tus ventas
             </p>
 
--- src/Components/Home/IntroMessage.jsx
+++ src/Components/Home/IntroMessage.jsx
@@ -3,12 +3,12 @@ import Button from "../Button";
 
 const IntroMessage = () => {
   return (
-    <div className="responsive-padding md:mb-10">
-        <p className="md:text-xl lg:text-lg 2xl:text-2xl mb-10 xl:text-xl">
+    <div className="responsive-padding mb-4 sm:mb-5 md:mb-10 mt-4">
+        <p className="text-sm sm:text-xl lg:text-lg 2xl:text-2xl mb-5 sm:mb-10 xl:text-xl">
         Puede sonar aburrido, así que queremos hacerlo divertido. Este el punto de partida para que tomes acción. Ya sabes que necesitas. Un sitio web con power, una  marca que se haga notar, y estrategias  que pateen traseros. Déjalo en nuestras manos, y prepárate para conquistar el mundo digital.
         </p>
         <div className="text-center">
-        <Button text={'Quiero comenzar con mi página web'} paddingX={'md:px-4 lg:px-3 xl:px-4'} paddingY={'md:py-2 lg:py-1 xl:py-2'} fontSize={'md:text-xl lg:text-lg xl:text-xl'}/>
+        <Button text={'Quiero comenzar con mi página web'} paddingX={'px-1 sm:px-3 md:px-4 lg:px-3 xl:px-4'} paddingY={'py-1 sm:py-1 md:py-2 lg:py-1 xl:py-2'} fontSize={'text-sm sm:text-lg md:text-xl lg:text-lg xl:text-xl'}/>
         </div>
         
     </div>
